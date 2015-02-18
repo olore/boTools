afe2e413f5fa16688b9fc686297cdaa5d375e0e8
Brian Olore <brian.olore@adp.com>
2015-02-18 16:40:56 -0500

    identify 'scripts' extract runFunction

--- panel/bo-panel.js
+++ panel/bo-panel.js
@@ -1,18 +1,8 @@
 (function() {
 
+var scripts = { };
 
-function listen() {
-  var countWatchersButton = document.querySelector('.count-watchers'),
-      countScopesButton = document.querySelector('.count-scopes');
-
-  countWatchersButton.addEventListener('click', callCountAngularWatchers);
-  countScopesButton.addEventListener('click', callCountAngularScopes);
-}
-
-window.addEventListener('load', listen);
-
-function countScopes() {
-
+scripts.countScopes = function() {
   var i, data, scope,
         scopeList = [],
         seenScope = {},
@@ -27,29 +17,26 @@ function countScopes() {
     toProcess -= 1;
 
     if (!seenScope[scope.$id]){
-        scopes += 1;
-        maxScopeId = Math.max(scope.$id, maxScopeId);
-        seenScope[scope.$id] = true;
+      scopes += 1;
+      maxScopeId = Math.max(scope.$id, maxScopeId);
+      seenScope[scope.$id] = true;
     }
 
     if (scope.$$childHead !== null) {
-        toProcess += 1;
-        scopeList.push(scope.$$childHead);
+      toProcess += 1;
+      scopeList.push(scope.$$childHead);
     }
 
     if (scope.$$nextSibling !== null) {
-        toProcess += 1;
-        scopeList.push(scope.$$nextSibling);
+      toProcess += 1;
+      scopeList.push(scope.$$nextSibling);
     }
 
   }
-  
-  console.log('There are ', scopes, ' scopes');
-  console.log('The maximum scope id is ', maxScopeId);
   return scopes;
 }
 
-function countWatchers() {
+scripts.countWatchers = function() {
   var i, data, scope,
   count = 0,
   all = document.all,
@@ -69,26 +56,36 @@ function countWatchers() {
       }
     }
   }
-  var msg = 'This page has ' + count + ' angular watchers';
-  console.log(msg);
   return count;
 }
 
 function callCountAngularWatchers() {
-
-  chrome.devtools.inspectedWindow.eval('(' + countWatchers.toString() + ')()', function(output) {
-    console.log(output);
+  runFunction(scripts.countWatchers, function(output) {
     document.querySelector('.count-watchers-output').innerHTML = 'There are <b>' + output + '</b> watchers on the page';
   });
 }
 
 function callCountAngularScopes() {
-
-  chrome.devtools.inspectedWindow.eval('(' + countScopes.toString() + ')()', function(output) {
-    console.log(output);
+  runFunction(scripts.countScopes, function(output) {
     document.querySelector('.count-scopes-output').innerHTML = 'There are <b>' + output + '</b> scopes on the page';
   });
 }
+
+function runFunction(fn, cb) {
+  chrome.devtools.inspectedWindow.eval('(' + fn.toString() + ')()', cb);
+}
+
+
+
+function listen() {
+  var countWatchersButton = document.querySelector('.count-watchers'),
+      countScopesButton = document.querySelector('.count-scopes');
+
+  countWatchersButton.addEventListener('click', callCountAngularWatchers);
+  countScopesButton.addEventListener('click', callCountAngularScopes);
+}
+
+window.addEventListener('load', listen);
 
 
 })();
