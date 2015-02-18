e167dfd9ebfff9058906cbcd4edeeb55882280db
Brian Olore <brian.olore@adp.com>
2015-02-18 17:20:51 -0500

    refactor to be more generic

--- panel/bo-panel.js
+++ panel/bo-panel.js
@@ -2,90 +2,111 @@
 
 var scripts = { };
 
-scripts.countScopes = function() {
-  var i, data, scope,
-        scopeList = [],
-        seenScope = {},
-        toProcess = 1,
-        scopes = 0
-        maxScopeId = 0;
+scripts.countScopes = {
+  name: 'Count Scopes',
+  output: 'There are <b>{{output}}</b> scopes on the page',
+  outputSelector: '.count-scopes-output',
+  buttonSelector: '.count-scopes',
+  fn: function() {
+    var i, data, scope,
+          scopeList = [],
+          seenScope = {},
+          toProcess = 1,
+          scopes = 0
+          maxScopeId = 0;
 
-  scopeList.push(angular.element(document).injector().get('$rootScope'));
+    scopeList.push(angular.element(document).injector().get('$rootScope'));
 
-  while(toProcess !== 0) {
-    scope = scopeList.shift();
-    toProcess -= 1;
+    while(toProcess !== 0) {
+      scope = scopeList.shift();
+      toProcess -= 1;
 
-    if (!seenScope[scope.$id]){
-      scopes += 1;
-      maxScopeId = Math.max(scope.$id, maxScopeId);
-      seenScope[scope.$id] = true;
+      if (!seenScope[scope.$id]){
+        scopes += 1;
+        maxScopeId = Math.max(scope.$id, maxScopeId);
+        seenScope[scope.$id] = true;
+      }
+
+      if (scope.$$childHead !== null) {
+        toProcess += 1;
+        scopeList.push(scope.$$childHead);
+      }
+
+      if (scope.$$nextSibling !== null) {
+        toProcess += 1;
+        scopeList.push(scope.$$nextSibling);
+      }
+
     }
-
-    if (scope.$$childHead !== null) {
-      toProcess += 1;
-      scopeList.push(scope.$$childHead);
-    }
-
-    if (scope.$$nextSibling !== null) {
-      toProcess += 1;
-      scopeList.push(scope.$$nextSibling);
-    }
-
+    return scopes;
   }
-  return scopes;
 }
 
-scripts.countWatchers = function() {
-  var i, data, scope,
-  count = 0,
-  all = document.all,
-  len = all.length,
-  test = {};
+scripts.countWatchers = {
+  name: 'Count Watchers',
+  output: 'There are <b>{{output}}</b> watchers on the page',
+  outputSelector: '.count-watchers-output',
+  buttonSelector: '.count-watchers',
+  fn: function() {
+    var i, data, scope,
+    count = 0,
+    all = document.all,
+    len = all.length,
+    test = {};
 
-  // go through each element. Count watchers if it has scope or isolate scope
-  /* eslint no-for-loops:0 */
-  for (i = 0; i < len; i++) {
-    /* global angular */
-    data = angular.element(all[i]).data();
-    scope = data.$scope || data.$isolateScope;
-    if (scope && scope.$$watchers) {
-      if ( !test[ scope.$id ] ) {
-        test[ scope.$id ] = true;
-        count += scope.$$watchers.length;
+    // go through each element. Count watchers if it has scope or isolate scope
+    /* eslint no-for-loops:0 */
+    for (i = 0; i < len; i++) {
+      /* global angular */
+      data = angular.element(all[i]).data();
+      scope = data.$scope || data.$isolateScope;
+      if (scope && scope.$$watchers) {
+        if ( !test[ scope.$id ] ) {
+          test[ scope.$id ] = true;
+          count += scope.$$watchers.length;
+        }
       }
     }
+    return count;
   }
-  return count;
 }
 
-function callCountAngularWatchers() {
-  runFunction(scripts.countWatchers, function(output) {
-    document.querySelector('.count-watchers-output').innerHTML = 'There are <b>' + output + '</b> watchers on the page';
+function runScript(obj) {
+  runFunction(obj.fn, function(output) {
+    document.querySelector(obj.outputSelector).innerHTML = obj.output.replace('{{output}}', output);
   });
 }
 
-function callCountAngularScopes() {
-  runFunction(scripts.countScopes, function(output) {
-    document.querySelector('.count-scopes-output').innerHTML = 'There are <b>' + output + '</b> scopes on the page';
-  });
-}
-
+// I think this is bad, but I can't get message passing to work
 function runFunction(fn, cb) {
   chrome.devtools.inspectedWindow.eval('(' + fn.toString() + ')()', cb);
 }
 
 
 
-function listen() {
-  var countWatchersButton = document.querySelector('.count-watchers'),
-      countScopesButton = document.querySelector('.count-scopes');
+function setup() {
+  // Stupid forEach can't iterate on an object
+  ['countScopes', 'countWatchers'].forEach(function(scriptName) {
+    var scriptObj = scripts[scriptName],
+        btn = document.createElement('button'),
+        div = document.createElement('div');
 
-  countWatchersButton.addEventListener('click', callCountAngularWatchers);
-  countScopesButton.addEventListener('click', callCountAngularScopes);
+    // create button
+    btn.setAttribute('class', scriptObj.buttonSelector);
+    btn.innerHTML = scriptObj.name;
+    document.body.appendChild(btn);
+
+    // create area for output
+    div.setAttribute('class', scriptObj.outputSelector);
+    document.body.appendChild(div);
+
+    // setup listeners
+    document.querySelector(scriptObj.buttonSelector).addEventListener('click', runScript.bind(null, scriptObj));
+  });
 }
 
-window.addEventListener('load', listen);
+
+window.addEventListener('load', setup);
 
 
 })();
